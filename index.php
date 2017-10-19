b6aac9f8d4b811e73f37bcc5a2b67645e3c3a941
Faizal Hasanwala <faizal3199@users.noreply.github.com>
2017-10-19 17:24:56 +0530

    Syntax error resolved

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 	require_once('way2smsapi.php');
 	
 	//sendMessage($msg,$mobile,$username,$password)
-	sendMessage("<your_message>","<mobile_numbers_to_send>","<username>","<password>","verbose:true/false")
+	sendMessage("<your_message>","<mobile_numbers_to_send>","<username>","<password>","verbose:true/false");
 ?>
